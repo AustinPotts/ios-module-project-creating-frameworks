c8d2b5b17509af73088ca479d04d8475a2397c74
AustinPotts <pottsevolvements@gmail.com>
2020-02-27 08:00:05 -0500

    Configured Framework Import Project

---
+++ LoadingUI-XCODE/LoadingUI-XCODE/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LoadingUI-XCODE
+//
+//  Created by Austin Potts on 2/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import LoadingUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
